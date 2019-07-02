fbe78b0554dc1396b68ada483dcb884301422763
Greg <g.rutherford10@gmail.com>
2019-07-02 20:06:32 +0100

    add Button & Subtitle components

--- app/Main.js
+++ app/Main.js
@@ -1,81 +1,216 @@
+    
 import React from 'react';
+import {
+	StyleSheet,
+	View,
+	StatusBar,
+	ActivityIndicator,
+	ScrollView,
+	AsyncStorage
+} from 'react-native';
+import { LinearGradient } from 'expo';
+import uuid from 'uuid/v1';
+
+import { primaryGradientArray } from './utils/Colors';
 import Header from './components/Header';
+import SubTitle from './components/SubTitle';
 import Input from './components/Input';
 import List from './components/List';
-import { LinearGradient } from 'expo';
-import { primaryGradientArray } from './utils/Colors';
-import { StyleSheet, View, StatusBar, ScrollView } from 'react-native';
+import Button from './components/Button';
 
-const headerTitle = 'To Do';
+const headerTitle = 'Todo';
 
 export default class Main extends React.Component {
+	state = {
+		inputValue: '',
+		loadingItems: false,
+		allItems: {},
+		isCompleted: false
+	};
 
-    state = {
-        inputValue: ''
-    };
+	componentDidMount = () => {
+		this.loadingItems();
+	};
 
-    newInputValue = value => {
-        this.setState({
-            inputValue: value
-        })
-    };
+	newInputValue = value => {
+		this.setState({
+			inputValue: value
+		});
+	};
 
-    render(){
+	loadingItems = async () => {
+		try {
+			const allItems = await AsyncStorage.getItem('Todos');
+			this.setState({
+				loadingItems: true,
+				allItems: JSON.parse(allItems) || {}
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-        const { inputValue } = this.state;
+	onDoneAddItem = () => {
+		const { inputValue } = this.state;
+		if (inputValue !== '') {
+			this.setState(prevState => {
+				const id = uuid();
+				const newItemObject = {
+					[id]: {
+						id,
+						isCompleted: false,
+						text: inputValue,
+						createdAt: Date.now()
+					}
+				};
+				const newState = {
+					...prevState,
+					inputValue: '',
+					allItems: {
+						...prevState.allItems,
+						...newItemObject
+					}
+				};
+				this.saveItems(newState.allItems);
+				return { ...newState };
+			});
+		}
+	};
 
-        return(
-            <LinearGradient
-            colors={primaryGradientArray}
-            style={styles.container}
-          >
-            <StatusBar barStyle="light-content" />
-            <View style={styles.centered}>
-              <Header title={headerTitle} />
-            </View>
-            <View style={styles.inputContainer}>
-              <Input inputValue={inputValue} onChangeText={this.newInputValue} />
-            </View>
-            <View style={styles.list}>
-                <ScrollView contentContainerStyle={styles.scrollableList}>
-                    {Object.values(allItems)
-                    .reverse()
-                    .map(item => (
-                        <List key={item.id} 
-                        {...item} 
-                        deleteItem={this.deleteItem} 
-                        completeItem={this.completeItem}
-                        incompleteItem={this.incompleteItem}
-                        />
-                    ))
-                    }
-                </ScrollView>
-            </View>
-          </LinearGradient>
-        )
-    };
+	deleteItem = id => {
+		this.setState(prevState => {
+			const allItems = prevState.allItems;
+			delete allItems[id];
+			const newState = {
+				...prevState,
+				...allItems
+			};
+			this.saveItems(newState.allItems);
+			return { ...newState };
+		});
+	};
 
+	completeItem = id => {
+		this.setState(prevState => {
+			const newState = {
+				...prevState,
+				allItems: {
+					...prevState.allItems,
+					[id]: {
+						...prevState.allItems[id],
+						isCompleted: true
+					}
+				}
+			};
+			this.saveItems(newState.allItems);
+			return { ...newState };
+		});
+	};
+
+	incompleteItem = id => {
+		this.setState(prevState => {
+			const newState = {
+				...prevState,
+				allItems: {
+					...prevState.allItems,
+					[id]: {
+						...prevState.allItems[id],
+						isCompleted: false
+					}
+				}
+			};
+			this.saveItems(newState.allItems);
+			return { ...newState };
+		});
+	};
+
+	deleteAllItems = async () => {
+		try {
+			await AsyncStorage.removeItem('Todos');
+			this.setState({ allItems: {} });
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	saveItems = newItem => {
+		const saveItem = AsyncStorage.setItem('Todos', JSON.stringify(newItem));
+	};
+
+	render() {
+		const { inputValue, loadingItems, allItems } = this.state;
+
+		return (
+			<LinearGradient colors={primaryGradientArray} style={styles.container}>
+				<StatusBar barStyle="light-content" />
+				<View style={styles.centered}>
+					<Header title={headerTitle} />
+				</View>
+				<View style={styles.inputContainer}>
+					<SubTitle subtitle={"What's Next?"} />
+					<Input
+						inputValue={inputValue}
+						onChangeText={this.newInputValue}
+						onDoneAddItem={this.onDoneAddItem}
+					/>
+				</View>
+				<View style={styles.list}>
+					<View style={styles.column}>
+						<SubTitle subtitle={'Recent Notes'} />
+						<View style={styles.deleteAllButton}>
+							<Button deleteAllItems={this.deleteAllItems} />
+						</View>
+					</View>
+
+					{loadingItems ? (
+						<ScrollView contentContainerStyle={styles.scrollableList}>
+							{Object.values(allItems)
+								.reverse()
+								.map(item => (
+									<List
+										key={item.id}
+										{...item}
+										deleteItem={this.deleteItem}
+										completeItem={this.completeItem}
+										incompleteItem={this.incompleteItem}
+									/>
+								))}
+						</ScrollView>
+					) : (
+						<ActivityIndicator size="large" color="white" />
+					)}
+				</View>
+			</LinearGradient>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    centered: {
-        alignItems: 'center'
-    },
-    container: {
-        flex: 1,
-    },
-    inputContainer: {
-        marginTop: 40,
-        paddingLeft: 15,
-        alignItems: 'center'
-    },
-    list: {
-        flex: 1,
-        marginTop: 70,
-        paddingLeft: 15,
-        marginBottom: 10
-      },
-      scrollableList: {
-        marginTop: 15
-      },
-})
+	container: {
+		flex: 1
+	},
+	centered: {
+		alignItems: 'center'
+	},
+	inputContainer: {
+		marginTop: 40,
+		paddingLeft: 15
+	},
+	list: {
+		flex: 1,
+		marginTop: 70,
+		paddingLeft: 15,
+		marginBottom: 10
+	},
+	scrollableList: {
+		marginTop: 15
+	},
+	column: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between'
+	},
+	deleteAllButton: {
+		marginRight: 40
+	}
+});
